4b1e9eee55460fd7b753f0f20f7906015f600d3e
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-13 15:33:02 +0100

    feat: structure js e html

---
+++ Ciclo for spesa/js/script.js
@@ -0,0 +1,15 @@
+// Data una lista della spesa:
+// const list = [
+//     'Latte',
+//     'Cacao',
+//     'Insalata',
+//     'Carote',
+//     'Pomodori',
+//     'Piatti pronti'
+// ];
+// Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+
+// lista spesa
+// scorrere le lista della spesa
+// stampare gli elementi singoli nel DOM
+    // prendere l'elemento dal DOM e appendere la lista
---
+++ readme.md
@@ -0,0 +1,3 @@
+Data una lista della spesa:
+Stampare sulla pagina gli elementi della lista individualmente.
+Svolgere questo esercizio in 2 versioni: col ciclo for e col ciclo while.
